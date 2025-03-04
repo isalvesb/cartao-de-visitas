e4b5bf2fe6835a6ca84c0f33e4b67caa93f2d43b
Isa Alves <alvesisamara254@gmail.com>
2025-03-04 12:01:13 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 const infos = {
-    nome: 'Isa Alves',
+    nome: 'Isamara Alves',
+    rm: 'RM565161',
     cargo: 'Estudante de Web Design',
     imagem: 'https://github.com/isalvesb.png',
     github: 'https://github.com/isalvesb',
     linkedin: 'https://www.linkedin.com/in/isamara-alves-0b75902aa/',
-    minibio: 'Isa Alves é fotógrafa e tratadora de imagem, com vasta experiência na captura e edição de fotografias, sempre buscando realçar a beleza e a essência de cada momento através do olhar sensível e da técnica apurada. Formada como técnica em Processos Fotográficos, aprimorou suas habilidades tanto no estúdio quanto no tratamento digital, desenvolvendo um olhar criativo e detalhista para a composição visual.
-    Atualmente, cursa Web Design na FIAP, onde expande seu repertório criativo ao explorar o universo digital, integrando suas habilidades visuais à criação de interfaces e experiências digitais envolventes.Apaixonada pela estética e pela comunicação visual, busca unir suas duas paixões para entregar projetos completos e impactantes.',
+    minibio: 'Isa Alves tem 25 anos, é fotógrafa e tratadora de imagem, com vasta experiência na
+          captura e edição de fotografias, sempre buscando realçar a beleza e a
+          essência de cada momento através do olhar sensível e da técnica
+          apurada.Formada como técnica em Processos Fotográficos, aprimorou
+          suas habilidades tanto no estúdio quanto no tratamento digital,
+    desenvolvendo um olhar criativo e detalhista para a composição visual.
+        Atualmente, cursa Web Design na FIAP, onde expande seu repertório
+          criativo ao explorar o universo digital, integrando suas habilidades
+          visuais à criação de interfaces e experiências digitais envolventes.
+          Apaixonada pela estética e pela comunicação visual, busca unir suas
+          duas paixões para entregar projetos completos e impactantes.
+          Seus hobbies são ler, jogar videogame e treinar.',
 };
 
 document.getElementById('imagem').src = infos.imagem;
